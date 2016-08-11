8f7f54f468d285559c2cc165dd8aea9c015e3a26
mhassaan <mhassaanchaudhry@gmail.com>
2016-08-11 20:09:12 +0200

    Redirect the User To Index Page With Message Project Not Found In Case of Missing Record.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: [:show,:edit,:update,:destroy]
+
   def index
     @projects = Project.all
   end
@@ -9,8 +11,8 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find params[:id]
   end
+
   def create
     @project = Project.new(project_parmas)
     if @project.save
@@ -23,11 +25,9 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find params[:id]
   end
 
   def update
-    @project = Project.find params[:id]
     if @project.update(project_parmas)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -38,7 +38,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find params[:id]
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
@@ -48,6 +47,13 @@ class ProjectsController < ApplicationController
 
   def project_parmas
     params.require(:project).permit(:name,:description)
+  end
+
+  def set_project
+    @project = Project.find params[:id]
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
   end
 
 end
